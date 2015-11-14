3a569e1c033b4ec086255b4fedb93ea3b1635901
Amman <amadeusz.opac@gmail.com>
2015-11-14 12:23:51 +0100

    Initial copy

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SubtitleConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Property = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wczytaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(222, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "label9";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.ColumnHeadersVisible = false;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Property,
+            this.Value});
+            this.dgv.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgv.Location = new System.Drawing.Point(93, 12);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(103, 316);
+            this.dgv.TabIndex = 4;
+            // 
+            // Property
+            // 
+            this.Property.HeaderText = "";
+            this.Property.Name = "Property";
+            this.Property.ReadOnly = true;
+            this.Property.Width = 50;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            this.Value.Width = 50;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 340);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Subtitle Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Property;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubtitleConverter
+{
+    public partial class Form1 : Form
+    {
+        private string address;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            
+           
+            var ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            var path = ofd.FileName;
+            address = path;
+            var bufor = "";
+            bufor = File.ReadAllText(path);
+            dgv.Rows.Add(13);
+            dgv.Rows[0].Cells[0].Value = "Linii"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[0].Cells[1].Value = bufor.Count(x => x == '\n');
+            
+            dgv.Rows[1].Cells[0].Value = "ć"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[1].Cells[1].Value = bufor.Count(x => x == 'æ');
+
+            dgv.Rows[2].Cells[0].Value = "ę"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[2].Cells[1].Value = bufor.Count(x => x == 'ê');
+            
+            dgv.Rows[3].Cells[0].Value = "ś"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[3].Cells[1].Value = bufor.Count(x => x == 'œ');
+            
+            dgv.Rows[4].Cells[0].Value = "Ś"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[4].Cells[1].Value = bufor.Count(x => x == 'Œ');
+            
+            dgv.Rows[5].Cells[0].Value = "ż"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[5].Cells[1].Value = bufor.Count(x => x == '¿');
+            
+            dgv.Rows[6].Cells[0].Value = "Ż"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[6].Cells[1].Value = bufor.Count(x => x == '¯');
+
+            dgv.Rows[7].Cells[0].Value = "ą"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[7].Cells[1].Value = bufor.Count(x => x == '¹');
+            
+            dgv.Rows[8].Cells[0].Value = "ń"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[8].Cells[1].Value = bufor.Count(x => x == 'ñ');
+            
+            dgv.Rows[9].Cells[0].Value = "ł"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[9].Cells[1].Value = bufor.Count(x => x == '³');
+            
+            dgv.Rows[10].Cells[0].Value = "Ł"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[10].Cells[1].Value = bufor.Count(x => x == '£');
+            
+            dgv.Rows[11].Cells[0].Value = "ź"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[11].Cells[1].Value = bufor.Count(x => x == 'Ÿ');
+            
+            dgv.Rows[12].Cells[0].Value = "Ź"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[12].Cells[1].Value = "No data";//bufor.Count(x => x == '£');
+            
+            dgv.Rows[13].Cells[0].Value = "XX"; //ILE LINII ||| LINIA -> LINII
+            dgv.Rows[13].Cells[1].Value = "XX";//bufor.Count(x => x == '£');
+            
+
+            
+            
+            var om = bufor.Count(x => x == '¹');
+            var ni = bufor.Count(x => x == 'ñ');
+            var uy = bufor.Count(x => x == '³');
+
+
+            // ê - ę | œ - ś | ¿ - ż | ¹ - ą | ñ - ń | ³ - ł
+            /*
+            a1.Text = nextLine.ToString();
+            a2.Text = ci.ToString();
+            a3.Text = em.ToString();
+            a4.Text = si.ToString();
+            a5.Text = rzy.ToString();
+            a6.Text = om.ToString();
+            a7.Text = ni.ToString();
+            a8.Text = uy.ToString();
+             */
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (address == "") return;
+            var bufor = "";
+            bufor = File.ReadAllText(address);
+            // ê - ę | œ - ś | ¿ - ż | ¹ - ą | ñ - ń | ³ - ł | ¯ - Ż | Œ - Ś | Ÿ - ź |  - Ź | £ - Ł
+            bufor = bufor.Replace('æ', 'ć');
+            bufor = bufor.Replace('ê', 'ę');
+            bufor = bufor.Replace('œ', 'ś');
+            bufor = bufor.Replace('¿', 'ż');
+            bufor = bufor.Replace('¹', 'ą');
+            bufor = bufor.Replace('ñ', 'ń');
+            bufor = bufor.Replace('³', 'ł');
+            bufor = bufor.Replace('Ÿ', 'ź');
+            bufor = bufor.Replace('Œ', 'Ś');
+            bufor = bufor.Replace('¯', 'Ż');
+            bufor = bufor.Replace('£', 'Ł');
+
+            var savePath = address;
+
+            File.WriteAllText(savePath,bufor);
+            label9.Text = "done";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label9.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label9.Text = "bgfhsdjbgdjk";
+        }
+    }
+}
